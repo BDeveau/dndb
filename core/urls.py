cb0a4e3ffad7edc99077ac61d0337ad36c600c11
Brad <Brad@clearsoftware.com>
2017-02-08 17:24:49 -0500

    First Commit

--- core/urls.py
+++ core/urls.py
@@ -6,11 +6,11 @@ admin.autodiscover()
 import hello.views
 
 # Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
+# url(r'^$', 'core.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', db.views.index, name='index'),
+    url(r'^db', db.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
