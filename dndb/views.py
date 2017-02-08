b32988035711891a91727ee918ff32f9c160a94f
Brad <Brad@clearsoftware.com>
2017-02-08 17:39:42 -0500

    UGH

--- dndb/views.py
+++ dndb/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from dndb import Greeting
+from .models import Greeting
 
 # Create your views here.
 def index(request):
