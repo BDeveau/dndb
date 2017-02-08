c4585ec192f2bb0fb0d32778f9a286da063233ba
Brad <Brad@clearsoftware.com>
2017-02-08 17:32:38 -0500

    Typo

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import db.views
 
 # Examples:
 # url(r'^$', 'core.views.home', name='home'),
