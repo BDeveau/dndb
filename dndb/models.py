860c275e049a416951fa1d13c94a66a6926306d3
Brad <Brad@clearsoftware.com>
2017-02-08 20:16:43 -0500

    allow null

--- dndb/models.py
+++ dndb/models.py
@@ -4,18 +4,18 @@ from django.db import models
 
 class Location(models.Model):
     location_name = models.CharField(max_length=50)
-    location_text = models.TextField(blank=True)
-    location_location = models.ForeignKey('self', blank=True, on_delete=models.CASCADE)
+    location_text = models.TextField(null=True, blank=True)
+    location_location = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.location_name
     
 class Character(models.Model):
     character_name = models.CharField(max_length=50)
-    character_race = models.CharField(max_length=20, blank=True)
-    character_sex = models.CharField(max_length=10, blank=True)
-    character_text = models.TextField(blank=True)
-    character_location = models.ForeignKey(Location, blank=True, on_delete=models.CASCADE)
+    character_race = models.CharField(max_length=20, null=True, blank=True)
+    character_sex = models.CharField(max_length=10, null=True, blank=True)
+    character_text = models.TextField(null=True, blank=True)
+    character_location = models.ForeignKey(Location, null=True, blank=True, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.character_name
