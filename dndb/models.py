4efec55a1615742e2e63d4ab120ac4cc476ee13d
Brad <Brad@clearsoftware.com>
2017-02-08 19:44:50 -0500

    Strings for models an Admin registration

--- dndb/models.py
+++ dndb/models.py
@@ -7,9 +7,15 @@ class Location(models.Model):
     location_text = models.TextField(blank=True)
     location_location = models.ForeignKey('self', blank=True, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return self.location_name
+    
 class Character(models.Model):
     character_name = models.CharField(max_length=50)
     character_race = models.CharField(max_length=20, blank=True)
     character_sex = models.CharField(max_length=10, blank=True)
     character_text = models.TextField(blank=True)
     character_location = models.ForeignKey(Location, blank=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.character_name
