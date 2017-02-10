bb9bd40c2b4cf26c8d74bae383a8e2e7ced14330
brad <brad@brad-work>
2017-02-10 16:19:29 -0500

    add campaign to admin

--- dndb/admin.py
+++ dndb/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Location, Character
+from .models import Location, Character, Campaign
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Character)
+admin.site.register(Campaign)
