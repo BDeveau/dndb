f29f14e437ac4d2b14c47177e703d217c27c9f31
brad <brad@brad-work>
2017-02-10 16:28:40 -0500

    does this work

--- dndb/models.py
+++ dndb/models.py
@@ -21,9 +21,9 @@ class Location(models.Model):
     
 class Character(models.Model):
     name = models.CharField(max_length=50)
-    race = models.CharField(max_length=20, null=True, blank=True)
+    race = models.CharField(max_length=50, null=True, blank=True)
     sex = models.CharField(max_length=10, null=True, blank=True)
-    title = models.CharField(max_length=20, null=True, blank=True)
+    title = models.CharField(max_length=50, null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
     location = models.ForeignKey(Location, null=True, blank=True, on_delete=models.CASCADE)
     campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
