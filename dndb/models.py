9674ed55095017112d8c60197174de71c14a1d37
brad <brad@brad-work>
2017-02-10 15:30:48 -0500

    Let's see what this does...

--- dndb/models.py
+++ dndb/models.py
@@ -1,21 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Campaign(models.Model):
+    name = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
+
 class Location(models.Model):
-    location_name = models.CharField(max_length=50)
-    location_text = models.TextField(null=True, blank=True)
-    location_location = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    notes = models.TextField(null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.location_name
     
 class Character(models.Model):
-    character_name = models.CharField(max_length=50)
-    character_race = models.CharField(max_length=20, null=True, blank=True)
-    character_sex = models.CharField(max_length=10, null=True, blank=True)
-    character_text = models.TextField(null=True, blank=True)
-    character_location = models.ForeignKey(Location, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    race = models.CharField(max_length=20, null=True, blank=True)
+    sex = models.CharField(max_length=10, null=True, blank=True)
+    title = models.CharField(max_length=20, null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    location = models.ForeignKey(Location, null=True, blank=True, on_delete=models.CASCADE)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.character_name
