df8f4d620a0bac5965ce749fadd759f6bf2ed1b9
Brad <Brad@clearsoftware.com>
2017-02-16 21:48:22 -0500

    add task to admin

--- dndb/admin.py
+++ dndb/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Location, Character, Campaign
+from .models import Location, Character, Campaign, Task
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Character)
 admin.site.register(Campaign)
+admin.site.register(Task)
