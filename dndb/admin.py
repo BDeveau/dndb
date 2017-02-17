e680903c76bee3c35111630712d6ca52f61da518
Brad <Brad@clearsoftware.com>
2017-02-16 22:48:16 -0500

    partyloot to admin

--- dndb/admin.py
+++ dndb/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Location, Character, Campaign, Task
+from .models import Location, Character, Campaign, Task, PartyLoot
 
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Character)
 admin.site.register(Campaign)
 admin.site.register(Task)
+admin.site.register(PartyLoot)
