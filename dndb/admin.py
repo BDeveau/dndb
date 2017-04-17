0d5dabd7e34a586a658586b31109cfcd3af75431
Brad <Brad@clearsoftware.com>
2017-04-17 17:13:08 -0400

    Modified Campaign User Admin

--- dndb/admin.py
+++ dndb/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 
 from .models import Location, Character, Campaign, Task, PartyLoot
 
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    filter_horizontal = ["users"]
+
 # Register your models here.
 admin.site.register(Location)
 admin.site.register(Character)
-admin.site.register(Campaign)
 admin.site.register(Task)
 admin.site.register(PartyLoot)
