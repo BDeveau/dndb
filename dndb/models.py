e2a188eb4d1d8c10eeab3d2285ae7639355ef378
Brad Deveau <BDeveau@Gmail.com>
2017-12-21 12:23:01 -0500

    zomg new user?

--- dndb/models.py
+++ dndb/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.forms import ModelForm
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.utils.translation import gettext as _
+from django.core import validators
+
 
 import sys
 
@@ -12,11 +15,82 @@ Party Loot
 """
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    A class implementing a fully featured User model with admin-compliant
+    permissions.
+
+    Email and password are required. Other fields are optional.
+    """
+
+    first_name = models.CharField(max_length=50, blank=True)
+
+    last_name = models.CharField(max_length=50, blank=True)
+
+    email = models.EmailField(
+        _('Email Address'), unique=True,
+        error_messages={
+            'unique': _("A user with that email already exists."),
+        }
+    )
+    username = models.CharField(
+        _('Username'), max_length=30, unique=True, blank=True, null=True,
+        help_text=_('30 characters or fewer. Letters, numbers and _ only.'),
+        validators=[
+            validators.RegexValidator(
+                r'^\w+$',
+                _('Enter a valid username. This value may contain only '
+                  'letters, numbers and _ character.'),
+                'invalid'
+            ),
+        ],
+        error_messages={
+            'unique': _("The username is already taken."),
+        }
+    )
+    is_staff = models.BooleanField(
+        _('Staff Status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.')
+    )
+    is_active = models.BooleanField(
+        _('Active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.')
+    )
+    date_joined = models.DateTimeField(_('Date Joined'), auto_now_add=True)
+
+    objects = UserManager()
+
     USERNAME_FIELD = 'email'
+
+    class Meta(object):
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+        abstract = False
+
+    def get_full_name(self):
+        """
+        Returns email instead of the fullname for the user.
+        """
+        return email_to_name(self.email)
+
+    def get_short_name(self):
+        """
+        Returns the short name for the user.
+        This function works the same as `get_full_name` method.
+        It's just included for django built-in user comparability.
+        """
+        return self.get_full_name()
 
     def __str__(self):
         return self.email
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
 class Campaign(models.Model):
