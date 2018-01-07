b92fafb0e99a5e9bfdbb22297e348a7b2d8e38ae
Brad Deveau <BDeveau@Gmail.com>
2018-01-07 14:42:20 -0500

    Fix User Admin Panel

--- dndb/admin.py
+++ dndb/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User, Location, Character, Campaign, Task, PartyLoot, Item, Post, Comment
 
 @admin.register(Campaign)
 class CampaignAdmin(admin.ModelAdmin):
     filter_horizontal = ["users"]
 
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ["date_joined"]
+    list_display = ["email", "username"]
+    search_fields = ["email", "username"]
+
 # Register your models here.
-admin.site.register(User, UserAdmin)
 admin.site.register(Location)
 admin.site.register(Character)
 admin.site.register(Task)
