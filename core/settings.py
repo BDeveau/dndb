b39f0914ccbcda1cfbd29c8900c49a3adb3e187c
Brad Deveau <BDeveau@Gmail.com>
2018-01-24 16:52:12 -0500

    Password Reset.

--- core/settings.py
+++ core/settings.py
@@ -20,6 +20,13 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
 AUTH_USER_MODEL = 'dndb.User'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bdeveau@gmail.com'
+EMAIL_HOST_PASSWORD = 'Redandblue2258'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
--- core/urls.py
+++ core/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': 'index'}, name='logout'),
     url(r'^password_reset/$', auth_views.password_reset, {'template_name': 'registration/change_password.html'}, name='password_reset'),
-    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, {'template_name': 'registration/reset_done.html'}, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
-    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+        auth_views.password_reset_confirm, {'template_name': 'registration/reset_confirm.html'}, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, {'template_name': 'registration/reset_complete.html'}, name='password_reset_complete'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
 
 
